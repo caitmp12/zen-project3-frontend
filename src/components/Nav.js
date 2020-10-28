f28d1baf0f757f7dfe9a39abf97bb9588e903dfc
Ida <idahuladki@yahoo.com>
2020-10-27 21:38:56 -0400

    nav a style update

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,11 +4,11 @@ const Nav = () => {
 
     return (
         <div className="nav">
-            <a href="#">H</a>
-            <a href="#">M</a>
-            <a href="#">D</a>
-            <a href="#">T</a>
-            <a href="#">F</a>
+            <a href="#"><ion-icon name="home-outline"></ion-icon></a>
+            <a href="#"><ion-icon name="videocam-outline"></ion-icon></a>
+            <a href="#"><ion-icon name="wine-outline"></ion-icon></a>
+            <a href="#"><ion-icon name="pizza-outline"></ion-icon></a>
+            <a href="#"><ion-icon name="star-outline"></ion-icon></a>
         </div>
     )
 }
