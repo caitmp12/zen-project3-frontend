d2bca2f9896b1c368d50e3d659ae2b527279bcc2
Ida <idahuladki@yahoo.com>
2020-10-30 13:08:13 -0400

    form styled, images added

--- src/components/Form.js
+++ src/components/Form.js
@@ -16,37 +16,50 @@ const Form = (props) => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        name="name"
-        value={formData.name}
-        placeholder="name"
-        onChange={handleChange}
-      />
-      <input
-        type="text"
-        name="img"
-        value={formData.img}
-        placeholder="Image URL"
-        onChange={handleChange}
-      />
-      <input
-        type="text"
-        name="ingredients"
-        value={formData.ingredients}
-        placeholder="Ingredients"
-        onChange={handleChange}
-      />
-      <input
-        type="text"
-        name="directions"
-        value={formData.directions}
-        placeholder="Directions"
-        onChange={handleChange}
-      />
-      <input type="submit" value={props.label} />
-    </form>
+    <div className="form-container">
+      <h2>Create Your New {`${type}`}</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="field-wrap">
+          <label>Name:</label><input
+            type="text"
+            name="name"
+            value={formData.name}
+            placeholder="Name"
+            onChange={handleChange}
+          />
+        </div>
+        <div className="field-wrap">
+          <label>Picture:</label><input
+            type="text"
+            name="img"
+            value={formData.img}
+            placeholder="Image URL"
+            onChange={handleChange}
+          />
+        </div>
+        <div className="field-wrap">
+          <label>Ingredients:</label><input
+            className="textarea-medium"
+            type="textarea"
+            name="ingredients"
+            value={formData.ingredients}
+            placeholder="Ingredients"
+            onChange={handleChange}
+          />
+        </div>
+        <div className="field-wrap">
+          <label>Directions:</label><input
+            className="textarea-large"
+            type="textarea"
+            name="directions"
+            value={formData.directions}
+            placeholder="Directions"
+            onChange={handleChange}
+          />
+        </div>
+        <input className="submit-button" type="submit" value={props.label.toUpperCase()} />
+      </form>
+    </div>
   );
 };
 
