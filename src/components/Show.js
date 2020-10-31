a873b04da4dce02f2b7832b9f65864c4a968d4c8
Ida <idahuladki@yahoo.com>
2020-10-30 20:40:43 -0400

    show page styled

--- src/components/Show.js
+++ src/components/Show.js
@@ -7,33 +7,37 @@ const Show = (props) => {
 
   return (
     <div>
-      <div className="index-container">
+      <div className="show-container">
+        <h2>{item.name}</h2>
         <div className="show-flex">
-          <img src={item.img} />
-          <h2>{item.name}</h2>
-          <ul>
-            {item.ingredients.map((ingredient) => (
-              <li>{ingredient}</li>
-            ))}
-          </ul>
-
-          <button
-            onClick={() => {
-              props.selectItem(item, type);
-              props.history.push(`/edit/${type}s`);
-            }}
-          >
-            Edit
-          </button>
-          <button
-            onClick={() => {
-              props.deleteItem(item, type);
-              props.history.push(`/${type}s`);
-            }}
-          >
-            Delete
-          </button>
-          <p></p>
+          <div className="show-row">
+            <img src={item.img} className="pic"/>
+            <div className="show-col">
+              <h3>Ingredients</h3>
+              <ul>
+                {item.ingredients.map((ingredient) => (
+                  <li>{ingredient}</li>
+                ))}
+              </ul>
+            </div>
+          </div>
+          <div className="show-row">
+            <p className="show-p">{item.directions}</p>
+          </div>
+          <div className="show-row edit-delete-buttons">
+            <button className="edit-delete"
+              onClick={() => {
+                props.selectItem(item, type);
+                props.history.push(`/edit/${type}s`);
+              }}>Edit
+            </button>
+            <button className="edit-delete"
+              onClick={() => {
+                props.deleteItem(item, type);
+                props.history.push(`/${type}s`);
+              }}>Delete
+            </button>
+          </div>
         </div>
       </div>
     </div>
